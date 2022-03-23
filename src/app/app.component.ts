579f332649f436df12fbaaa765c03a8384dfe2ef
knimperase <kn@impera.se>
2022-03-23 09:31:54 +0100

    file upload and csv parser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgxCsvParser, NgxCSVParserError } from 'ngx-csv-parser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'i-z382';
+  header: boolean = false;
+  csvRecords: any;
+
+  constructor(private ngxCsvParser: NgxCsvParser) {}
+
+  public changeListener(event: Event){
+    const element = event.currentTarget as HTMLInputElement;
+    let files: FileList = element.files as FileList;
+
+    this.header = (this.header as unknown as string) === 'true' || this.header === true;
+
+    this.ngxCsvParser.parse(files[0], { header: this.header, delimiter: ',' })
+      .pipe().subscribe({
+        next: (result): void => {
+          console.log('Result', result);
+          this.csvRecords = result;
+        },
+        error: (error: NgxCSVParserError): void => {
+          console.log('Error', error);
+        }
+      });
+  }
 }
